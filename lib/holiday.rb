bc0f2c79136d1e85a97b7d6db886d7286a098e88
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-17 14:20:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:summer][:fourth_of_july][1]
+  end
+
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -18,32 +21,45 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-end
+
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |season, day|
+    day.each do |key, supplies|
+      supplies << supply
+    end
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+  holiday_hash[:spring][:memorial_day] = [supply]
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+holiday_hash[season][holiday_name] = supply_array
+holiday_hash
+end
+
   # code here
   # remember to return the updated hash
 
-end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+  winter_supplies =[]
+  holiday_hash[:winter].map do |day, supplies|
+    winter_supplies << supplies
+    end
+winter_supplies = winter_supplies.flatten
 
 end
+# binding.pry
+
+  # return an array of all of the supplies that are used in the winter season
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -61,10 +77,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
