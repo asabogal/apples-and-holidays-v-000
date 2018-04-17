078cf3cae943768bf36c3463ea63f5ecc86a6e7f
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-17 14:43:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    
+
     holiday.each do |day, supplies|
       season_s = season.to_s.capitalize!
       day_s = day.to_s.capitalize!
